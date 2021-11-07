b75cb7f909ff6fd401cf7569e38ff67f33524888
Iskandar Umidov <iskander.umidov.iu@gmail.com>
2021-11-07 14:31:26 -0600

    loginstate change

--- Loginstate.java
+++ Loginstate.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Loginstate extends WarehouseState{
   private static final int CLIENT = 0;
   private static final int MANAGER = 1;
-  private static final int SALESPERSON = 2;
+  private static final int CLERK = 2;
   private static final int EXIT = 3;
 
 
@@ -58,16 +58,12 @@ public class Loginstate extends WarehouseState{
     }
     return true;
   }
-  //Wrong?
-  // private void client(){
-  //   (WarehouseContext.instance()).setLogin(WarehouseContext.IsClient);
-  //   (WarehouseContext.instance()).changeState(0);
-  // }
+
   private void client(){
-    String userID = getToken("Please input the manager id: ");
-    if (Warehouse.instance().searchMembership(userID) != null){
-      (WarehouseContext.instance()).setLogin(WarehouseContext.IsUser);
-      (WarehouseContext.instance()).setUser(userID);      
+    String clientID = getToken("Please input the manager id: ");
+    if (Warehouse.instance().searchClients(clientID) != null){
+      (WarehouseContext.instance()).setLogin(WarehouseContext.IsClient);
+      (WarehouseContext.instance()).setClient(clientID);      
       (WarehouseContext.instance()).changeState(0);
     }
     else 
@@ -75,32 +71,19 @@ public class Loginstate extends WarehouseState{
   } 
 
   private void manager(){
-    String userID = getToken("Please input the manager id: ");
-    if (Warehouse.instance().searchMembership(userID) != null){
-      (WarehouseContext.instance()).setLogin(WarehouseContext.IsUser);
-      (WarehouseContext.instance()).setUser(userID);      
-      (WarehouseContext.instance()).changeState(1);
-    }
-    else 
-      System.out.println("Invalid user id.");
-  } 
-
-  private void salesperson(){
-    String userID = getToken("Please input the salesperson id: ");
-    if (Warehouse.instance().searchMembership(userID) != null){
-      (WarehouseContext.instance()).setLogin(WarehouseContext.IsUser);
-      (WarehouseContext.instance()).setUser(userID);      
-      (WarehouseContext.instance()).changeState(2);
-    }
-    else 
-      System.out.println("Invalid user id.");
-  } 
+    (WarehouseContext.instance()).setLogin(WarehouseContext.IsManager);
+    (WarehouseContext.instance()).changeState(1);
+  }
+  private void clerk(){
+    (LibContext.instance()).setLogin(LibContext.IsClerk);
+    (LibContext.instance()).changeState(2);
+  }
 
   public void process() {
     int command;
       System.out.println("Please input 0 to login as Client\n"+ 
                         "input 1 to login as Manager\n" +
-                        "input 2 to login as SalesPerson\n" +
+                        "input 2 to login as Clerk\n" +
                         "input 3 to exit the system\n");  
     while ((command = getCommand()) != EXIT) {
 
@@ -109,14 +92,14 @@ public class Loginstate extends WarehouseState{
                                 break;
         case MANAGER:        manager();
                                 break;
-        case SALESPERSON:        salesperson();
+        case CLERK:        clerk();
                                 break;
         default:                System.out.println("Invalid choice");
 
       }
       System.out.println("Please input 0 to login as Client\n"+ 
                         "input 1 to login as Manager\n" +
-                        "input 2 to login as SalesPerson\n" +
+                        "input 2 to login as Clerk\n" +
                         "input 3 to exit the system\n"); 
     }
     (WarehouseContext.instance()).changeState(3);
